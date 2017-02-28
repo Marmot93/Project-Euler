ff15d33af352989bf1c9ca0df9095d84e17ca377
Marmot93 <654332905@qq.com>
2017-02-28 13:24:57 +0800

    012

--- 012.py
+++ 012.py
@@ -8,10 +8,12 @@ def sanjiao(n):
 count = 0
 n = 1
 while count <= 500:
-    count = 1
     n += 1
+    count = 1   #下面可能要减 先加一个，最后减
     for i in range(1, int(sqrt(sanjiao(n)))):
         if not sanjiao(n) % i:
             count += 2
-
+    if int(sqrt(sanjiao(n))) == sqrt(sanjiao(n)) : # 刚好是平方数 减一
+        count -= 1
 print('答案是： '+ str(sanjiao(n)))
+print('因数个数是： ' + str(count - 1))
