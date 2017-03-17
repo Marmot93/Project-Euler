22dfd445d4c0e6b46e49570a7e4ea3c37eb1ee1d
Marmot93 <654332905@qq.com>
2017-03-17 19:21:45 +0800

    025

---
+++ 1-50/025.py
@@ -0,0 +1,13 @@
+print("""斐波那契数列中第一个包含 1000 位数字的项是第几项？""")
+
+
+list_feb = [1, 1]
+count = 2
+while (True):
+    n = list_feb[-1] + list_feb[-2]
+    if len(str(n)) == 1000:
+        print(count+1)      #循环到这个的时候break但是计数还是得+1
+        break
+    else:
+        list_feb.append(n)
+        count +=1
