9b407eaa88ea83451657a8696c372ca378dbf040
Marmot93 <654332905@qq.com>
2017-03-17 19:58:40 +0800

    026

---
+++ 1-50/026.py
@@ -0,0 +1,18 @@
+print("""找出小于 1000 的数字 d，1/d 的十进制表示含有最长的循环圈""")
+
+# 扩10取余
+def len_is(x):
+    b = []
+    a = 10 % x
+    while a not in (b):
+        b.append(a)
+        a = (a*10) % x
+    return len(b)
+# 找最长
+max_num = 0
+max_len = 0
+for d in range(1,1000):
+    if len_is(d) > max_len:
+        max_len = len_is(d)
+        max_num = d
+print (max_num)
