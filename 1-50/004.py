adaeff7b9c94d9440647570d14d9fb042f81ef68
Marmot93 <654332905@qq.com>
2017-03-30 00:00:59 +0800

    更新004方法3

--- 1-50/004.py
+++ 1-50/004.py
@@ -13,13 +13,13 @@ for x in range(999,100,-1):
         if num == num_1:   #判定字符串相等
             list_huiwen.append(int(num))
 
-print("答案是： " + str(max(list_huiwen)) )
+print("答案是： " , str(max(list_huiwen)) )
 
 print("""
 方法二：压缩为一行代码
 ------------------------------------------------""")
 num = (max([i*j for i in range(100,1000) for j in range(100,1000) if str(i*j) == str(i*j)[::-1]]))
-print("答案是： " + str(num))
+print("答案是： " , str(num))
 
 
 print("""
@@ -30,4 +30,4 @@ def is_palindrome(n):
 
 
 output = filter(is_palindrome, [x * y for x in range(100, 1000) for y in range(100, 1000)])
-print(max(list(output)))
+print('答案是：',max(list(output)))
