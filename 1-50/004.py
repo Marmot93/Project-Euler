836f09e67350e29befe56c49c49a250b61628809
Marmot93 <654332905@qq.com>
2017-06-05 22:08:27 +0800

    更新004方法3

--- 1-50/004.py
+++ 1-50/004.py
@@ -1,33 +1,34 @@
-print("""一个回文数指的是从左向右和从右向左读都一样的数字。
+"""一个回文数指的是从左向右和从右向左读都一样的数字。
 最大的由两个两位数乘积构成的回文数是 9009 = 91 * 99。
 找出最大的有由个三位数乘积构成的回文数。
-------------------------------------------------""")
+"""
 
 # 反向取乘积，换成字符串，反向分片,若相等得解,加入列表，取max
 
-list_huiwen = []  #回文数列表
-for x in range(999,100,-1):
+list_huiwen = []  # 回文数列表
+for x in range(999, 100, -1):
     for y in range(999, 100, -1):
         num = str(x * y)
         num_1 = num[::-1]
-        if num == num_1:   #判定字符串相等
+        if num == num_1:  # 判定字符串相等
             list_huiwen.append(int(num))
 
-print("答案是： " , str(max(list_huiwen)) )
+print("答案是： ", str(max(list_huiwen)))
 
 print("""
 方法二：压缩为一行代码
 ------------------------------------------------""")
-num = (max([i*j for i in range(100,1000) for j in range(100,1000) if str(i*j) == str(i*j)[::-1]]))
-print("答案是： " , str(num))
-
+num = (max([i * j for i in range(100, 1000) for j in range(100, 1000) if str(i * j) == str(i * j)[::-1]]))
+print("答案是： ", str(num))
 
 print("""
 方法三：filter筛选
 ------------------------------------------------""")
+
+
 def is_palindrome(n):
     return str(n) == str(n)[::-1]
 
 
 output = filter(is_palindrome, [x * y for x in range(100, 1000) for y in range(100, 1000)])
-print('答案是：',max(list(output)))
+print('答案是：', max(list(output)))
